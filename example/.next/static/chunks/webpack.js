36a62e726da0226f83c9a41be9d86fe7570d6750
Sean Aguinaga <aguinaga.sean@gmail.com>
2021-07-23 09:50:15 -0700

    should be done

--- example/.next/static/chunks/webpack.js
+++ example/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ded849510689e2f4204"; }
+/******/ 		__webpack_require__.h = function() { return "c936b32964ef02566a84"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
